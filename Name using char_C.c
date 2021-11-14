ae4a1f7fa03be959460c328fd1e18918957d2cfa
shubhamD88 <94123771+shubhamD88@users.noreply.github.com>
2021-11-14 16:30:52 +0530

    Name using Char
    Input name using Char

---
+++ Name using char_C.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+printf("Enter your name :");
+char name[10];
+    scanf("%s",&name);
+    printf("you just say that your name is : %s", name);
+   
+    return 0;
+}
